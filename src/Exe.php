52522b7bdce2af5974b84b51711f998e2b3c11d9
JaservTECH <jafarabdurrahmanal-basyir@hotmail.com>
2019-06-26 21:34:28 +0700

    add Initial first stage

---
+++ src/Exe.php
@@ -0,0 +1,29 @@
+<?php
+namespace JFileExplorer;
+/*
+Declare By      : Jafar Abdurrahman Albasyir
+Email           : jafarabdurrahmanalabsyir@gmail.com
+Description     : Main class to execute the whole system file
+*/
+class Exe 
+{
+    //define variable
+    private static $Obj;                        //private variable instance of class
+    /*
+    disable free instance
+    make this class singleton by access function static Obj
+    */
+
+    //define methode
+    private function __CONSTRUCT()
+    {
+
+    }
+
+    //function gateway to get the instance class
+    public static function Obj()
+    {
+
+    }
+}
+?>
